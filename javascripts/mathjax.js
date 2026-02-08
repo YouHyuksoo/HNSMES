af264e06b85462e5c5da8b6c70e60f6a82d0ad1b
YouHyuksoo <hsyou@jisheng.co.kr>
2026-02-09 06:52:21 +0900

    Deployed b36fa38 with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,15 @@
+/**
+ * MathJax Configuration for HNSMES Documentation
+ */
+
+window.MathJax = {
+  tex: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]'], ['$$', '$$']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
+  }
+};
